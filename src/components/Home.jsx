fe10ae1c5209096cfa111eb6ef090c692a851946
Herby <herbyosbourne31@gmail.com>
2023-01-11 07:41:11 -0500

    update css

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -12,17 +12,22 @@ const Home = () => {
         <main className="page">
           <div></div>
           <div className="intro">
-            <p className="tracking-in-expand-fwd">Hi, my name is</p>
+            <p className="tracking-in-expand-fwd welcome">Hi, my name is</p>
             <h1 className="name">
               <span className="first">Herby</span> <span className="last">Osbourne</span>
             </h1>
-            <div className="title">I build FrontEnd web Apps</div>
-            <div className="subtitle">
-              i'm a Front-End developer who skilled in React and javascript, a popular
-              JavaScript library for building user interfaces.
-              <br /> I'm able to take a wireframe, which is a visual representation of a
-              website or app's layout and design, and turn it into a fully functional app.
-            </div>
+            <p className="title">I build Front-End Web Apps</p>
+            <span className="subtitle">
+              <p>
+                i'm a Front-End developer who skilled in React and javascript, a popular
+                JavaScript library for building user interfaces.
+              </p>
+              {/* <br /> */}
+              <p>
+                I'm able to take a wireframe, which is a visual representation of a
+                website or app's layout and design, turn it into a fully functional app.
+              </p>
+            </span>
             <button className="cta-btn">Contact ME</button>
           </div>
           <div></div>
